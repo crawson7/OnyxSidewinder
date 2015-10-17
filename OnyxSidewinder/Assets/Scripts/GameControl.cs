ada13520ad768152291b7be552259dbe2ac15d65
crawson7 <crawson7@gmail.com>
2015-10-17 14:16:22 -0600

    Initial Project Import

---
+++ OnyxSidewinder/Assets/Scripts/GameControl.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        Debug.Log("Start here.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
